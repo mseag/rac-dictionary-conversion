fd5d4e6340ab8097634f895734a05de86f84bc87
Robin Munn <rmunn@pobox.com>
2020-01-16 15:03:48 +0700

    First version of conversion script
    This one is basically exploring the data file and identifying any places
    where the data is formatted differently than we might expect.

---
+++ convert.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import fileinput
+import re
+
+def fix_badly_wrapped_lines(lastline, line):
+    if re.match(r"^\s", line) and not re.search("/", line):
+        return lastline.rstrip() + " " + line.lstrip()
+    else:
+        return line.rstrip()
+
+def process(line):
+    # Break line down into headword, pronunciation, etc.
+    # TODO: Write this
+    pass
+
+lastline = ""
+for line in fileinput.input():
+    fixed_line = fix_badly_wrapped_lines(lastline, line)
+    process(fixed_line)
+    lastline = line
